8475133d980efe69cd3fe39217a30ebb580f9d56
Cancain <eriksson.tomas@gmail.com>
2019-09-22 00:51:11 +0200

    completed variables

---
+++ variables/app.py
@@ -0,0 +1,8 @@
+price = 10
+rating = 4.9
+name = "Tomas"
+is_published = True
+array = [1, 2, 3, 4, 5]
+
+for number in array:
+    print(number)
---
+++ variables/input.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+color = input("What is your favorite color? ")
+print("Hi " + name + " Your favorite color is " + color)
+
